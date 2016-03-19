dad7324101f23248da23c16d2b8a3478399b4f71
ryosukee <tmcit.miyazaki@gmail.com>
2016-03-19 20:53:30 +0900

    [add] initial commit

---
+++ example.py
@@ -0,0 +1,26 @@
+import time
+import random
+from multiprocessing import Process
+from multi_progressbar import ProgressManager
+
+
+def hoge(p):
+    for _ in p('Progress1', order=1, maxv=2, nest=0)(range(2)):
+        for _ in p('Progress2', order=2, maxv=3, nest=1)(range(3)):
+            for _ in p('Progress3', order=3, maxv=3, nest=2)(range(3)):
+                time.sleep(random.random())
+        for _ in p('Progress4', order=4, maxv=3, nest=1)(range(3)):
+            time.sleep(random.random())
+
+m = ProgressManager()
+progress_tree = m.new_tree()
+progress_tree2 = m.new_tree(offset=5)
+args = [progress_tree, progress_tree2]
+processes = list()
+for i in range(2):
+    p = Process(target=hoge, args=(args[i],))
+    processes.append(p)
+    p.start()
+for p in processes:
+    p.join()
+m.finish()
